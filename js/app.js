eba16312ebb8e26c45f1598f0624503b29beaeae
HimadriPaul24 <himadripaul821@gmail.com>
2021-09-14 20:03:49 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const showProducts = (products) => {
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h3>${product.title.slice(0, 50)}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: ${product.price}</h2>
      
